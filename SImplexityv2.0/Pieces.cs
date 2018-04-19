7da31a6d1fb5324d6e5646f0665ec5f7f534a274
fs000 <flaviosantos002010hotmail.com>
2018-04-19 19:58:27 +0100

    Adicionar enumeração com o tipo de peças

---
+++ SImplexityv2.0/Pieces.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SImplexityv2._0 {
+    enum PieceTypes {
+            RedSquare = 0,
+            RedCircle = 1,
+            WhiteSquare = 2,
+            WhiteCircle = 3,
+            Bar
+        }
+}
