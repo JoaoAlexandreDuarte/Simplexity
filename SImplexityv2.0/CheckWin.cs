8946cac24293e41358b2ad7f6b4a1a60de0cc1c3
fs000 <flaviosantos002010@hotmail.com>
2018-04-21 23:30:35 +0100

    Adição do método CheckVertical() e identação de código

--- SImplexityv2.0/CheckWin.cs
+++ SImplexityv2.0/CheckWin.cs
@@ -1,68 +1,109 @@
 ﻿using System;
 
 namespace SImplexityv2._0 {
+
     /// <summary>
     /// Checks all wining conditions
     /// </summary>
     public class CheckWin {
-        // String that takes in the information about who won
+
+        /// <summary>
+        /// String that takes in the information about which player won.
+        /// </summary>
         public static string win = "";
 
-        // Check shape in horizontal.
-        public static string CheckHorizontal(PieceTypes[,] grid)
-        {
+        /// <summary>
+        /// Checks if four pieces are in a row, horizontally.
+        /// </summary>
+        /// <param name="grid">Stores the contents of the grid.</param>
+        /// <returns></returns>
+        public static string CheckHorizontal(PieceTypes[,] grid) {
+
+            // 'Empty' variables
             int piecesInRow = 0;
             string shape = "";
             string color = "";
 
-            // Check shape in horizontal.
-            for (int i = 6; i >= 0; i--)
-            {
-                for (int j = 6; j > 0; j--)
-                {
+            // Check Shape.
+            for (int i = 6; i >= 0; i--) {
+                for (int j = 6; j > 0; j--) {
                     piecesInRow++;
                     shape = CheckShape(grid[i, j]);
-                    if (grid[j, i] == PieceTypes.Bar || grid[i, j - 1] == PieceTypes.Bar)
-                    {
+                    if (grid[i, j] == PieceTypes.Bar || grid[i, j - 1] == PieceTypes.Bar) {
+                        piecesInRow = 0;
+                    } else if (shape != CheckShape(grid[i, j - 1])) {
                         piecesInRow = 0;
                     }
-                    else if (shape != CheckShape(grid[i, j - 1]))
-                    {
-                        piecesInRow = 0;
-
-                    }
-                    if (piecesInRow == 3)
-                    {
+                    if (piecesInRow == 3) {
                         win = shape == "Square" ? "Player 2 Wins!" : "Player 1 Wins!";
                         return win;
                     }
                 }
-
             }
 
-            // Check color in horizontal.
-            for (int i = 6; i >= 0; i--)
-            {
-                for (int j = 6; j > 0; j--)
-                {
+            // Check Color.
+            for (int i = 6; i >= 0; i--) {
+                for (int j = 6; j > 0; j--) {
                     piecesInRow++;
                     color = CheckColor(grid[i, j]);
-                    if (grid[j, i] == PieceTypes.Bar || grid[i, j - 1] == PieceTypes.Bar)
-                    {
+                    if (grid[i, j] == PieceTypes.Bar || grid[i, j - 1] == PieceTypes.Bar) {
+                        piecesInRow = 0;
+                    } else if (color != CheckColor(grid[i, j - 1])) {
                         piecesInRow = 0;
                     }
-                    else if (color != CheckColor(grid[i, j - 1]))
-                    {
-                        piecesInRow = 0;
-
-                    }
-                    if (piecesInRow == 3)
-                    {
+                    if (piecesInRow == 3) {
                         win = color == "Red" ? "Player 2 Wins!" : "Player 1 Wins!";
                         return win;
                     }
                 }
+            }
+            return win;
+        }
 
+        /// <summary>
+        /// Checks if four pieces are in a row, vertically.
+        /// </summary>
+        /// <param name="grid">Stores the contents of the grid.</param>
+        /// <returns></returns>
+        public static string CheckVertical(PieceTypes[,] grid) {
+
+            // 'Empty' variables
+            int piecesInRow = 0;
+            string shape = "";
+            string color = "";
+
+            // Check Shape.
+            for (int i = 6; i >= 0; i--) {
+                for (int j = 6; j > 0; j--) {
+                    piecesInRow++;
+                    shape = CheckShape(grid[j, i]);
+                    if (grid[j, i] == PieceTypes.Bar || grid[j - 1, i] == PieceTypes.Bar) {
+                        piecesInRow = 0;
+                    } else if (shape != CheckShape(grid[j - 1, i])) {
+                        piecesInRow = 0;
+                    }
+                    if (piecesInRow == 3) {
+                        win = shape == "Square" ? "Player 2 Wins!" : "Player 1 Wins!";
+                        return win;
+                    }
+                }
+            }
+
+            // Check Color.
+            for (int i = 6; i >= 0; i--) {
+                for (int j = 6; j > 0; j--) {
+                    piecesInRow++;
+                    color = CheckColor(grid[j, i]);
+                    if (grid[j, i] == PieceTypes.Bar || grid[j - 1, i] == PieceTypes.Bar) {
+                        piecesInRow = 0;
+                    } else if (color != CheckColor(grid[j - 1, i])) {
+                        piecesInRow = 0;
+                    }
+                    if (piecesInRow == 3) {
+                        win = color == "Red" ? "Player 2 Wins!" : "Player 1 Wins!";
+                        return win;
+                    }
+                }
             }
             return win;
         }
@@ -71,17 +112,13 @@ namespace SImplexityv2._0 {
         /// Verifies if the value of each piece is divisible by two,
         /// in order to determine its shape.
         /// </summary>
-        /// <param name="currentPiece"></param>
+        /// <param name="currentPiece">Currently selected piece.</param>
         /// <returns></returns>
-        private static string CheckShape(PieceTypes currentPiece)
-        {
+        private static string CheckShape(PieceTypes currentPiece) {
             string shape = "";
-            if ((int)currentPiece % 2 == 0)
-            {
+            if ((int)currentPiece % 2 == 0) {
                 shape = "Square";
-            }
-            else if ((int)currentPiece % 2 != 0)
-            {
+            } else if ((int)currentPiece % 2 != 0) {
                 shape = "Circle";
             }
             return shape;
@@ -91,7 +128,7 @@ namespace SImplexityv2._0 {
         /// Verifies if the value of each piece is the same
         /// as the one on the 'enum', to determine its color.
         /// </summary>
-        /// <param name="currentPiece"></param>
+        /// <param name="currentPiece">Currently selected piece.</param>
         /// <returns></returns>
         private static string CheckColor(PieceTypes currentPiece) {
             string color = "";
