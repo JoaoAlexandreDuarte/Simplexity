b4ad723486ba27945b83f5d345b0b0d4b8026093
Leandro Brás <shadowgaming.7@gmail.com>
2018-04-21 12:32:54 +0100

    Adicionados comentários à classe 'Program'

--- SImplexityv2.0/Program.cs
+++ SImplexityv2.0/Program.cs
@@ -3,7 +3,9 @@
 namespace SImplexityv2._0 {
     class Program {
         static void Main(string[] args) {
+            // Changes the Console encoding to be 'UTF8'
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // Calls the 'DrawMenu()' Method from the 'Menu' Class
             Menu.DrawMenu();
         }
     }
