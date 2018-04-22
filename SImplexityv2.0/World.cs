456b3e081f3155e5b46bcf785beb39edddac8645
Leandro Brás <shadowgaming.7@gmail.com>
2018-04-22 17:19:22 +0100

    Apagado codigo morto, e comentada toda a classe 'World'

--- SImplexityv2.0/World.cs
+++ SImplexityv2.0/World.cs
@@ -33,6 +33,7 @@ namespace SImplexityv2._0 {
         /// Fill the grid with "|".
         /// </summary>
         private void PopulateEmptyGrid() {
+            // Runs the array filling each 'cell' with a 'PiceType.Bar'
             for (int row = 0; row < Rows; row++) {
                 for (int column = 0; column < Columns; column++) {
                     grid[row, column] = PieceTypes.Bar;
@@ -41,40 +42,31 @@ namespace SImplexityv2._0 {
         }
 
         /// <summary>
-        /// Checks if the board is full.
-        /// </summary>
-        /// <returns></returns>
-        public bool IsFull() {
-            for (int column = 0; column < Columns; column++) {
-                if (CanAdd(column)) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        /// <summary>
         /// Adds the chosen Piece to the chosen Column.
         /// </summary>
+        /// <param name="column">Number of the selected column</param>
+        /// <param name="currentPiece">Currently selected piece</param>
         public void Add(int column, PieceTypes currentPiece) {
             // Check if the chosen column is full.
             if (!CanAdd(column)) { return; }
 
-            // Starting at the top, move down to check for the correct position
-            // to assign the chosen Piece.
+            /* Starting at the top, move down to check for the correct position
+             * to assign the chosen Piece. */
             int currentRow = 0;
             while (currentRow < Rows - 1 &&
                 grid[currentRow + 1, column] == PieceTypes.Bar) {
                 currentRow++;
             }
 
-            // Place the chosen piece when it get to the right Row.
+            // Place the chosen piece when it gets to the right Row.
             grid[currentRow, column] = currentPiece;
         }
 
         /// <summary>
         /// Checks if the chosen piece can be added to the chosen Row.
         /// </summary>
+        /// <param name="column">Number of the selected column</param>
+        /// <returns>True or False, depending on the state of the column</returns>
         public bool CanAdd(int column) {
             if (grid[0, column] == PieceTypes.Bar) {
                 return true;
@@ -83,22 +75,32 @@ namespace SImplexityv2._0 {
             }
         }
 
-         public string Win(string result) {
+        /// <summary>
+        /// Checks all winning conditions to determine if anyone won
+        /// and if so who won.
+        /// </summary>
+        /// <param name="result">Stores the information about who won</param>
+        /// <returns>Winning condition</returns>
+        public string Win(string result) {
             result = CheckWin.CheckHorizontal(grid, result);
             result = CheckWin.CheckVertical(grid, result);
             result = CheckWin.CheckDiagonalLeft(grid, result);
             result = CheckWin.CheckDiagonalRight(grid, result);
             return result;
-        } 
+        }
 
         /// <summary>
         /// Draws the complete Board to the console.
         /// </summary>
         public void DrawBoard() {
+            // Runs the array
             for (int row = 0; row < Rows; row++) {
                 Console.Write("  ");
                 for (int column = 0; column < Columns; column++) {
                     PieceTypes type = grid[row, column];
+
+                    /* Check what type of pice is contained on that "cell" to draw
+                     * the corresponding piece on to the console*/
                     switch (type) {
                         case PieceTypes.Bar:
                             Console.ForegroundColor = ConsoleColor.Gray;
@@ -122,8 +124,10 @@ namespace SImplexityv2._0 {
                             break;
                     }
                 }
+                // Switch Line after completing each Row
                 Console.WriteLine("");
             }
+            // Draw the base line.
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("  ---------------------");
         }
